eb98d5149c4b391c97f7ec8e7178cb152fdff92b
EvertonWallace <everton542@hotmail.com>
2024-06-17 19:31:23 -0300

    configuracao das rotas picture no app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ require("./db/db_config.js")
 
 const port = process.env.PORT || 3000;
 
+const pictureRouter = require("./routes/picture.route.js");
+
+app.use("/pictures", pictureRouter);
+
 app.listen(port, () => {
   console.log(`O servidor está rodando na porta ${port}`);
 });
